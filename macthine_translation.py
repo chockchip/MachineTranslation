ed0fa73ca3f56b4bcf41c3f97a356d54373c56f6
chockchip <w.wattcharapong@gmail.com>
2020-10-22 23:50:31 +0700

    prepare data

--- macthine_translation.py
+++ macthine_translation.py
@@ -25,7 +25,6 @@ lines.target=lines.target.apply(lambda x: re.sub("'", '', x))
 
 # create a set of all special characters
 special_characters= set(string.punctuation)
-print(set(string.punctuation))
 
 # Remove all the special characters
 lines.source = lines.source.apply(lambda x: ''.join(char1 for char1 in x if char1 not in special_characters))
@@ -43,5 +42,129 @@ lines.source=lines.source.apply(lambda x: re.sub(" +", " ", x))
 lines.target=lines.target.apply(lambda x: re.sub(" +", " ", x))
 
 # Add start and end tokens to target sequences
+"""
+Doc
+Add START_ and _END token to the target sentence is very useful for training and during inference. 
+These tags help to know when to start the translation and when to end the translation.
+"""
 lines.target = lines.target.apply(lambda x : 'START_ '+ x + ' _END')
-print(lines.sample(6))
+
+# Find all the source and target words and sort them
+# -- Vocabulary of Source language
+all_source_words=set()
+for source in lines.source:
+    for word in source.split():
+        if word not in all_source_words:
+            all_source_words.add(word)
+# -- Vocabulary of Target 
+all_target_words=set()
+for target in lines.target:
+    for word in target.split():
+        if word not in all_target_words:
+            all_target_words.add(word)
+# -- sort all unique source and target words
+source_words= sorted(list(all_source_words))
+target_words=sorted(list(all_target_words))
+
+#Find maximum sentence length in  the source and target data
+source_length_list=[]
+for l in lines.source:
+    source_length_list.append(len(l.split(' ')))
+max_source_length= max(source_length_list)
+print(" Max length of the source sentence",max_source_length)
+target_length_list=[]
+for l in lines.target:
+    target_length_list.append(len(l.split(' ')))
+max_target_length= max(target_length_list)
+print(" Max length of the target sentence",max_target_length)
+
+# creating a word to index(word2idx) for source and target
+source_word2idx= dict([(word, i+1) for i,word in enumerate(source_words)])
+target_word2idx=dict([(word, i+1) for i, word in enumerate(target_words)])
+
+#creating a dictionary for index to word for source and target vocabulary
+source_idx2word= dict([(i, word) for word, i in  source_word2idx.items()])
+print(source_idx2word)
+target_idx2word =dict([(i, word) for word, i in target_word2idx.items()])
+
+#Shuffle the data
+lines = shuffle(lines)
+
+# Train - Test Split
+X, y = lines.source, lines.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1)
+X_train.shape, X_test.shape
+
+# Input tokens for encoder
+num_encoder_tokens=len(source_words)
+# Input tokens for decoder zero padded
+num_decoder_tokens=len(target_words) +1
+
+def generate_batch(X = X_train, y = y_train, batch_size = 128):
+    ''' Generate a batch of data '''
+    while True:
+        for j in range(0, len(X), batch_size):
+            encoder_input_data = np.zeros((batch_size, max_source_length),dtype='float32')
+            decoder_input_data = np.zeros((batch_size, max_target_length),dtype='float32')
+            decoder_target_data = np.zeros((batch_size, max_target_length, num_decoder_tokens),dtype='float32')
+            for i, (input_text, target_text) in enumerate(zip(X[j:j+batch_size], y[j:j+batch_size])):
+                for t, word in enumerate(input_text.split()):
+encoder_input_data[i, t] = source_word2idx[word] 
+                for t, word in enumerate(target_text.split()):
+                    if t<len(target_text.split())-1:
+                        decoder_input_data[i, t] = target_word2idx[word] # decoder input seq
+                    if t>0:
+                        # decoder target sequence (one hot encoded)
+                        # does not include the START_ token
+                        # Offset by one timestep
+                        #print(word)
+                        decoder_target_data[i, t - 1, target_word2idx[word]] = 1.
+                    
+            yield([encoder_input_data, decoder_input_data], decoder_target_data)
+
+
+train_samples = len(X_train)
+val_samples = len(X_test)
+batch_size = 128
+epochs = 50
+latent_dim=256
+
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None,))
+enc_emb =  Embedding(num_encoder_tokens, latent_dim, mask_zero = True)(encoder_inputs)
+encoder_lstm = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder_lstm(enc_emb)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None,))
+dec_emb_layer = Embedding(num_decoder_tokens, latent_dim, mask_zero = True)
+dec_emb = dec_emb_layer(decoder_inputs)
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(dec_emb,
+                                     initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that takes encoder and decoder input 
+# to output decoder_outputs
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+model.compile(optimizer=’rmsprop’, loss=’categorical_crossentropy’, metrics=[‘acc’])
+
+train_samples = len(X_train) # Total Training samples
+val_samples = len(X_test)    # Total validation or test samples
+batch_size = 128
+epochs = 100
+
+model.fit_generator(generator = generate_batch(X_train, y_train, batch_size = batch_size),
+                    steps_per_epoch = train_samples//batch_size,
+                    epochs=epochs,
+                    validation_data = generate_batch(X_test, y_test, batch_size = batch_size),
+                    validation_steps = val_samples//batch_size)
+
+model.save_weights(‘nmt_weights_100epochs.h5’)
